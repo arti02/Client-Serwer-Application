b8e903397d6216e824669915d6e2d5fa1d1cf68b
arti021998 <arti021998@gmail.com>
2021-04-25 20:18:24 +0200

    Initalize

--- Project_Tpum/Tpum/Prezentacja/MainWindow.xaml.cs
+++ Project_Tpum/Tpum/Prezentacja/MainWindow.xaml.cs
@@ -12,20 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfTest.ViewModels;
 
-namespace WpfTest
+namespace Prezentacja
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
-		
-
 		}
 	}
 }

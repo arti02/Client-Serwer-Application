7a219fe5a0fdde6c6d014ac39d87112ee64fc3d7
MichalSzczekocki <michal.szczekocki.poznan@gmail.com>
2021-04-26 11:36:40 +0200

    Logic test

---
+++ Project_Tpum/Tpum/LogicUnitTests/ProcessTemperatureTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LogicUnitTests
+{
+    [TestClass]
+    public class ProcessTemperatureTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void GetIDTest()
+        {
+        }
+        [TestMethod]
+        public void GetTest()
+        {
+        }        
+    }
+}
---
+++ Project_Tpum/Tpum/LogicUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogicUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71b201a8-8c11-4e02-a6fe-e8fd7b26a296")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

63dcf6dccc892599363dd45eb9c3156824e7515f
MichalSzczekocki <michal.szczekocki.poznan@gmail.com>
2021-04-26 11:36:27 +0200

    Data test

---
+++ Project_Tpum/Tpum/DataUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1925fa4-933e-4983-96ad-2f41fdef091a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project_Tpum/Tpum/DataUnitTests/RoomTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DataUnitTests
+{
+    [TestClass]
+    public class RoomTest
+    {
+        [TestMethod]
+        public void Setup()
+        {
+        }
+        [TestMethod]
+        public void GetIDTest()
+        {
+        }
+        [TestMethod]
+        public void GetTest()
+        {
+        }
+    }
+}

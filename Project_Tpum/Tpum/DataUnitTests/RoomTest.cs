bdcc5b17c88972da9b102725d3b962c10c8374d2
MichalSzczekocki <michal.szczekocki.poznan@gmail.com>
2021-05-04 00:22:19 +0200

    Dodanie testów Moq dla Logic i Data

--- Project_Tpum/Tpum/DataUnitTests/RoomTest.cs
+++ Project_Tpum/Tpum/DataUnitTests/RoomTest.cs
@@ -1,22 +1,32 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Moq;
+using Data;
 
 namespace DataUnitTests
 {
-    [TestClass]
+    [TestClass()]
     public class RoomTest
     {
-        [TestMethod]
-        public void Setup()
+        private readonly Mock<ARoom> _room_mock = new Mock<ARoom>();
+
+        [TestMethod()]
+        public void getActualTemperatureRangeTest()
         {
+            var mock = new Moq.Mock<ARoom>();
+            mock.Setup(m => m.getActualTemperature()).Returns(20);
+            Assert.AreEqual(20, mock.Object.getActualTemperature());
+            mock.Verify(m => m.getActualTemperature());
         }
-        [TestMethod]
-        public void GetIDTest()
+        [TestMethod()]
+        public void getRoomNameTest()
         {
-        }
-        [TestMethod]
-        public void GetTest()
-        {
+            var mock2 = new Moq.Mock<ARoom>();
+            mock2.Setup(m2 => m2.getRoomName()).Returns("Bathroom");
+            Assert.IsNotNull(mock2.Object.getRoomName());
+            mock2.Verify(m2 => m2.getRoomName());
         }
     }
 }
